63df8ff0c94e05836dc2affadac700f5841ad6fe
Jthui95 <jonathanhui.hk@gmail.com>
2020-06-30 00:59:29 -0700

    Add a loop code but couldnt figure how to make it output a password.

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var choice= Math.floor(Math.random()*4)
-var password = genPassword[choice]
 //Mywork
-function genPassword(){
-const array = [ "~!@#$%^&*()_+{}|-=[]\;',./?><:","1234567890","ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz",];
+function passwordSelection(){
+  const array = [ "~!@#$%^&*()_+{}|-=[]\;,./?><:","1234567890","ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz",];
+var sp = array["~!@#$%^&*()_+{}|-=[]\;,./?><:"];
+var num = array["1234567890"];
+var up = array["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lo = array["abcdefghijklmnopqrstuvwxyz"];
+let password = "";
+let i = 0;
+// create variable for each index
+//make a loop that will populate my string based on 
 // console.log(array);
-
-
-var pLength = prompt('How long would you like your password to be?');
-if (pLength > 8 && pLength < 128 ){
-  alert(`Your password will include ${pLength} characters`);
+var userP = prompt('How long would you like your password to be?');
+// while (userP <= userP){
+if (userP > 8 && userP < 128 ){
+  alert(`Your password will include ${userP} characters`);
 }
 else {
   alert('Invalid input, please choose a number between 8 and 128');
@@ -18,7 +23,6 @@ else {
 }
 var confirmLower = confirm("Would you like to include lower case letters?");
 if (confirmLower){
- 
   alert("Lower case letters will be included in your password." );
 }
 else {
@@ -26,10 +30,9 @@ else {
   array.splice(3,1);
 }
 // console.log(confirmLower);
-console.log(array); 
+// console.log(array); 
 var confirmUpper = confirm("Would you like to include upper case letters?");
 if (confirmUpper){
- 
   alert("Upper case letters will be included in your password.");
 }
 else {
@@ -37,10 +40,9 @@ else {
   array.splice(2,1);
 } 
 // console.log(confirmUpper);
-console.log(array); 
+// console.log(array); 
 var confirmNumeric = confirm("Would you like to include numbers?");
 if (confirmNumeric) {
- 
   alert("Numbers will be included in your password.");
 }
 else {
@@ -48,7 +50,7 @@ else {
   array.splice(1,1);
 }
 // console.log(confirmNumeric);
-console.log(array); 
+// console.log(array); 
 var confirmSpecial = confirm("Would you like to include special characters?");
 if (confirmSpecial){
   alert("Special characters will be included in your password.");
@@ -58,24 +60,62 @@ else {
   array.splice(0,1);
 }
 // console.log(confirmSpecial);
-console.log(array); 
+// console.log(array); 
+function generatePassword() {
+  while ( i> 8 && i < 128){
+    if (confirmLower === true){
+      password += lo[Math.floor(Math.random()*26)];
+      i++;
+    }
+    if (confirmUpper === true){
+      password += password = up[Math.floor(Math.random()*26)];
+      i++;
+    }
+    if (confirmNumeric === true){
+      password += num[Math.floor(Math.random()*10)];
+      i++;
+    }
+    if (confirmSpecial === true){
+      password += sp[Math.floor(Math.random()*19)];
+      i++;
+    }
+    console.log(password);
+    return password;
+  }
+//   var password = toString(genPassword);
+//   var passwordText = document.querySelector("#password");
+//   var passLength = userP
+//   var choice = Math.floor(Math.random()* passLength)
+// var password = ""
+
+//   for (var i = 0; i< choice; i++)
+//  password += array.charAt(Math.floor(Math.random()* array.length))
+// return password;
 }
-generateBtn.addEventListener("click", genPassword);
+generateBtn.addEventListener("click", passwordSelection, generatePassword);
 
 
 
 
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword("#genPassword");
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = toString(genPassword);
+//   var passwordText = document.querySelector("#password");
+//   var passLength = userP
+//   var choice = Math.floor(Math.random()* passLength)
+// var password = ""
+
+//   for (var i = 0; i< choice; i++)
+//  password += array.charAt(Math.floor(Math.random()* array.length))
+// return password;
+  // passwordText.value = password;
  
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
